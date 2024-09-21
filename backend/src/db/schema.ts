d90f588d26a66b70fb8f6ff986aa191ed023e952
subbuwu <nsubbu2004@gmail.com>
2024-09-21 22:16:25 +0530

    init backend and frontend

---
+++ backend/src/db/schema.ts
@@ -0,0 +1,7 @@
+import { integer, pgTable, serial, text, timestamp } from 'drizzle-orm/pg-core';
+
+export const usersTable = pgTable('users', {
+  id: serial('id').primaryKey(),
+  name: text('name').notNull(),
+  email: text('email').notNull().unique()
+});
---
+++ backend/src/index.ts
@@ -0,0 +1,15 @@
+import express, { Request, Response } from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware to parse JSON
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello, Express with TypeScript!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
